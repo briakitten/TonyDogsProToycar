b618e722b5b5b6a3b228b90099a8be58dcfbd02c
loteque <loteque@bluebox.local>
2025-08-10 19:32:44 -0700

    car controller first pass fixed

--- car/car.gd
+++ car/car.gd
@@ -2,17 +2,18 @@ extends CharacterBody3D
 
 @export var speed = 10.0
 @export var rotation_speed = 30
-@export var mouse_cam_sensitivity = 0.005
-@export var joystick_cam_sensitivity = 0.1
+@export_range(0.0, 1.0, .001) var mouse_cam_sensitivity = 0.005
+@export_range(0.0, 1.0, .01) var joystick_cam_sensitivity = 0.1
+@export_range(-90.0, 0.0, 0.1, "radians_as_degrees") var cam_min_vert = -PI/2
+@export_range(0.0, 90.0, 0.1, "radians_as_degrees") var cam_max_vert = PI/4
 
-@onready var model = %GreyBoxCar
-@onready var cam_h = %CamH
-@onready var cam_v = %CamV
+@onready var cam_arm = %CamSpringArm
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var h_input = 0.0
-var v_input = 0.0
-var _mouse_input_dir
+
+
+func _capture_mouse():
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 func _ready():
@@ -22,55 +23,43 @@ func _ready():
 func _update_cam_rotation(event: InputEvent):     
     if !Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED: return
     if event is InputEventMouseMotion:
-        h_input = -event.relative.x * mouse_cam_sensitivity
-        v_input = -event.relative.y * mouse_cam_sensitivity
+        # rotation around Y
+        cam_arm.rotation.y -= event.relative.x * mouse_cam_sensitivity
+        cam_arm.rotation.y = wrapf(cam_arm.rotation.y, 0.0, TAU)
+        # rotation around X
+        cam_arm.rotation.x -= event.relative.y * mouse_cam_sensitivity
+        cam_arm.rotation.x = clamp(cam_arm.rotation.x, cam_min_vert, cam_max_vert)
 
 
-func _unhandled_input(event: InputEvent):
+func _unhandled_input(event: InputEvent):    
     _update_cam_rotation(event)
 
 
-func _capture_mouse():
-    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+func _update_joy_direction():
+    var _joy_input_direction = Input.get_vector("look_left", "look_right", "look_up", "look_down")
+    cam_arm.rotate_y(-_joy_input_direction.y * joystick_cam_sensitivity)
+    cam_arm.rotate_x(-_joy_input_direction.x * joystick_cam_sensitivity)
 
 
-func _release_mouse():
-    Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+func _process(_delta: float) -> void:
+    cam_arm.global_position = global_position
 
 
 func _physics_process(delta):
-
-    # joystick
-    _mouse_input_dir = Input.get_vector("look_left", "look_right", "look_up", "look_down")
-    cam_h.rotate_y(-_mouse_input_dir.x * joystick_cam_sensitivity)
-    cam_v.rotate_x(-_mouse_input_dir.y * joystick_cam_sensitivity)
-
-    # mouse
-    cam_h.rotate_y(h_input)
-    cam_v.rotate_x(v_input)
-
-    # limit rotation on the x axsis
-    cam_v.rotation.x = clamp(
-        cam_v.rotation.x,
-        deg_to_rad(-90),
-        deg_to_rad(45)
-    )
-    h_input = 0.0
-    v_input = 0.0
+    _update_joy_direction()
     
     # movement
     var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
-    var direction = (cam_h.transform.basis * Vector3(input_dir.x, 0, input_dir.y))
+    var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
+    direction = direction.rotated(Vector3.UP, cam_arm.global_rotation.y)
     if not is_on_floor():
         velocity.y -= gravity * delta
     if direction:
         velocity.x = direction.x * speed
         velocity.z = direction.z * speed
-        var key_rotation = lerp_angle(rotation.y, atan2(-direction.x, -direction.z), rotation_speed * delta)
-        rotation.y = key_rotation + h_input
+        var rot_y_lerped = lerp_angle(rotation.y, atan2(-direction.x, -direction.z), rotation_speed * delta)
+        rotation.y = rot_y_lerped
     else:
         velocity.x = move_toward(velocity.x, 0, speed)
         velocity.z = move_toward(velocity.z, 0, speed)
     move_and_slide()
-    
-    
